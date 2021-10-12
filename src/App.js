242f52c7087ff3758ba931a2e7dc5459e3ded32a
Combatd <combatdstar@gmail.com>
2021-10-11 18:38:51 -0700

    Add tag to App Comopnent

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>TV Series List</h1>
+        <p className="App intro">
+          Our first functional component
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <intro />
     </div>
   );
 }
