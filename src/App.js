9db3d42e21fd9442dd698850439a629d34da5865
Combatd <combatdstar@gmail.com>
2021-10-12 03:13:04 -0700

    Declare App state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import Intro from './components/Intro';
 import './App.css';
 
 function App() {
+  const state = {
+    series: []
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>TV Series List</h1>
         <Intro message='Here you can find all of your most loved series' />
+        The length of series array = {state.series.length}
       </header>
     </div>
   );
