75023c0cb84280609d1a2710216766d50388ecc4
Combatd <combatdstar@gmail.com>
2021-10-11 17:53:16 -0700

    Render a hellow world in the ReactDOM

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const greeting = React.createElement('h1', {}, 'Hello World!');
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+ReactDOM.render(greeting, document.querySelector('body'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
