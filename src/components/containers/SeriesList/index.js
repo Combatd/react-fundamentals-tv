57169bdc5e6bc73f988b3047326886555c1afa5a
Combatd <combatdstar@gmail.com>
2021-10-17 08:22:13 -0700

    Decouple SeriesListItem from SeriesList

--- src/components/containers/SeriesList/index.js
+++ src/components/containers/SeriesList/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './index.css';
 
+const SeriesListItem = (props) => {
+    const { series } = props;
+    return(
+        <li key={series.show.id}>
+            {series.show.name}
+        </li>
+    )
+}
+
 const SeriesList = (props) => {
     return (
         <div>
             <ul className="series-list">
                 {props.list.map((series) => {
-                    return(
-                        <li key={series.show.id}>
-                            {series.show.name}
-                        </li>
+                    return (
+                        <SeriesListItem series={series} />
                     )
                 })}
             </ul>
