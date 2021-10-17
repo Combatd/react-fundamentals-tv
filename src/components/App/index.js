381a999acc96637d3c89f1680ac0cb9b2c70ec0a
Combatd <combatdstar@gmail.com>
2021-10-16 17:30:18 -0700

    Decouple message to Series component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,26 +5,6 @@ import Series from '../containers/Series';
 import 'whatwg-fetch';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      series: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-    .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
-      this.setState({ series: data });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
-
   render() {
     return (
       <div className="App">
@@ -32,7 +12,6 @@ class App extends Component {
           <h1>TV Series List</h1>
         </header>
         <Intro message='Here you can find all of your most loved series' />
-          The length of series array = {this.state.series.length}
         <Series />
       </div>
     );
--- src/components/containers/Series/index.js
+++ src/components/containers/Series/index.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 
 class Series extends Component {
+    constructor() {
+        super();
+        this.state = {
+        series: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+        .then((response) => {
+        return response.json()
+        })
+        .then((data) => {
+        this.setState({ series: data });
+        })
+        .catch((err) => {
+        console.log(err);
+        });
+    }
+        
     render() {
         return (
             <div>
-                Series Container
+                The length of series array = {this.state.series.length}
             </div>
         );
     }
