cb1ac7d101b836bbf9a617d351ff53cec4e37761
Combatd <combatdstar@gmail.com>
2021-10-17 21:05:42 -0700

    Declare Loader Component

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,1 +1,16 @@
 import React, {Component} from 'react';
+import loaderSrc from '../../assets/gura-gura-a.gif';
+
+class Loader extends Component {
+    render() {
+        return (
+            <div>
+                <img 
+                alt='loader icon'
+                src={loaderSrc} />
+            </div>
+        )
+    }
+}
+
+export default Loader;
